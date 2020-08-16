8cca688b42e3a02074cdea0a324ab0f5fbc32cae
F14r30n <50636608+F14r30n@users.noreply.github.com>
2020-08-16 21:35:23 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
+// register service worker
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./serviceworker.js', { scope: './' }).then(function(reg) {
+
+        if(reg.installing) {
+            console.log('Service worker installing');
+        } else if(reg.waiting) {
+            console.log('Service worker installed');
+        } else if(reg.active) {
+            console.log('Service worker active');
+        }
+
+    }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+
+
 function f() {
     console.log("Hi")
 }
